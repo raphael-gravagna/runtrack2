ce6ddcefeb0d1e2a75e510e9ac2e7f55f1e929b0
raphael gravagna <raphael.gravagna@laplateforme.io>
2021-11-02 13:51:30 +0100

    desc

---
+++ jour01/job02/index.php
@@ -0,0 +1,16 @@
+<?php
+    $str= "LaPlateforme";    
+    $str2= "Vive ";
+    $str3= " !";
+        echo $str2 . $str . $str3; ///unefois//
+    $str4= "LaPlateforme";    
+    $str5= "Vive ";
+    $str6= " !";
+        echo $str5 . $str4 . $str6; ////deuxfois///
+
+        $val= 6;
+        echo $val +4;
+        $myBool = false;
+        echo $myBool;
+
+?>
---
+++ jour01/job03/index.php
@@ -0,0 +1,5 @@
+<?php
+    $notes = [10, 20]
+    echo $notes[1];
+
+?>
