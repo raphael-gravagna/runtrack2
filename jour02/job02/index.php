2d44fec41a9adb5c0718e6ecc621b650eae111dd
raphael gravagna <raphael.gravagna@laplateforme.io>
2021-11-03 14:54:47 +0100

    jour 2
    != n'est pas
    réfléchir à l'envers c'est bien

---
+++ jour02/job02/index.php
@@ -0,0 +1,9 @@
+<?php
+    for ($var = 0; $var <= 1337; $var = $var + 1) {
+        if($var != 26 && $var != 37 && $var != 88 && $var != 1111 && $var != 3233 ) {
+            echo "$var"."<br/>"; $var=$var;
+        }
+
+
+    }
+?>
