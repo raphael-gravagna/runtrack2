30e1794fbbb151e6f1fab0fa095891a470ed8b52
raphael gravagna <raphael.gravagna@laplateforme.io>
2021-11-03 13:53:40 +0100

    jour deux

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+    for ($var = 0; $var <= 1337; $var = $var + 1) {
+        echo "$var"."<br/>";
+        if($var == 42) {
+            echo "<u>"."<b>"."42"."</b>"."</u>"."</br>"; $var=$var;
+        }
+
+
+
+    }
+
+    
+        
+
+?>
