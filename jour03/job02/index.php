d0ec303cbba260a7dfccca949f2046dad8e2531a
raphael gravagna <raphael.gravagna@laplateforme.io>
2021-11-04 15:43:22 +0100

    nous allons mieux

---
+++ jour03/job02/index.php
@@ -0,0 +1,14 @@
+<?php
+$i = 0;
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+while(isset($str[$i])) {
+        echo $str[$i]; 
+        $i = $i +2;
+
+    }
+
+    
+
+
+
+?>
