40388da528a99cc4821fb46ba76b914d5b0d9f59
raphael gravagna <raphael.gravagna@laplateforme.io>
2021-11-05 16:56:18 +0100

    on verra ça ce week-end hein

---
+++ jour04/job05/index.php
@@ -0,0 +1,40 @@
+<?php
+$i = 0;
+foreach($_POST as $key => $Value) 
+
+if($value = "John" && $value == "Rambo") {
+    echo "C'est pas ma guerre";
+    }
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="index.php" method="post" class="form-example">
+        <div class="form-example">
+          <label for="username">Enter your username: </label>
+          <input type="text" name="name" id="name" required>
+        </div>
+        <div class="form-example">
+          <label for="password.">Enter your password: </label>
+          <input type="password" name="password" id="pawword" required>
+        </div>
+        <div class="form-example">
+          <input type="submit" value="Subscribe!">
+        </div>
+      </form>
+
+
+    
+</body>
+</html>
